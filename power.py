386acb7e4439a8f7603e25cb8ce6260253db556e
Antigs <davidantigha7@gmail.com>
2024-08-27 13:00:14 +0100

    file added

---
+++ power.py
@@ -0,0 +1,10 @@
+#function to multiple two numbers and display the outcome of the numbers
+
+def get_multiple(number_1=10,number_2=20):
+    print("find the multiple of two numbers")
+    multiply=number_1* number_2
+    print(number_1, "*", number_2, "=", multiply )
+    
+get_multiple()
+    
+     
